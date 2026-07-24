e02f62a94411f2877e52a22892c32d7d33695fc8
jnsougata <jnsougata@gmail.com>
2026-07-24 20:31:09 +0530

    Minor changes and add logo.

--- opentube/utils.py
+++ opentube/utils.py
@@ -1,7 +1,7 @@
 import json
 import re
 from collections import OrderedDict
-from typing import Dict, Any
+from typing import Any, Optional, List
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 
@@ -10,7 +10,13 @@ from .errors import TooManyRequests, InvalidURL, RequestError
 __all__ = ["dup_filter", "request", "extract_initial_data"]
 
 
-def request(url: str) -> str:
+def request(url: str) -> Any | None:
+    """
+    The base function for making a request with proper headers.
+
+    Args:
+        url (str): The url to make a request.
+    """
     headers = {
         "User-Agent": (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -32,7 +38,14 @@ def request(url: str) -> str:
         raise RequestError(f"{e!r}") from None
 
 
-def dup_filter(iterable: list, limit: int = None) -> list:
+def dup_filter(iterable: list, limit: Optional[int] = None) -> List[Any]:
+    """
+    Utility function for filtering out duplicate items.
+
+    Args:
+        iterable (list): The list of items to filter.
+        limit (Optional[int]): The maximum number of items to return.
+    """
     if not iterable:
         return []
     lim = limit if limit else len(iterable)
@@ -44,6 +57,12 @@ def dup_filter(iterable: list, limit: int = None) -> list:
 
 
 def extract_initial_data(html: str) -> Any:
+    """
+    Utility function for extracting the initial data from the HTML of a YouTube page.
+
+    Args:
+        html (str): The HTML of the YouTube page.
+    """
     pattern = re.compile("ytInitialData = {(.+?)};")
     results = pattern.finditer(html)
     return json.loads("{" + results.__next__().group(1) + "}")
