10697c92a8d0762db3d149cf1af3081cf8d59b4d
jnsougata <jnsougata@gmail.com>
2026-07-29 22:39:45 +0530

    add: docstring for `Short`

--- opentube/shorts.py
+++ opentube/shorts.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 from .https import request
 
@@ -14,12 +15,21 @@ def _extract_itemprop(prop: str, source: str, tag: str = "meta"):
 
 
 class Short:
+    """
+    Represents a YouTube Short video and provides methods to extract its metadata.
+    """
     def __init__(self, url: str):
         self.url = url
         self._html = request(self.url)
 
     @property
     def author(self):
+        """
+        Returns the author of the short video.
+
+        Returns:
+            Dict[str, str]: Author of the short video.
+        """
         matched = re.search(
             r'<span itemprop="author" itemscope itemtype="http://schema.org/Person">(.*?)</span>',
             self._html,
@@ -28,40 +38,88 @@ class Short:
             "name": _extract_itemprop("name", matched.group(1), "link"),
             "url": re.search(
                 r'<link itemprop="url" href="(.*?)">', matched.group(1)
-            ).group(1),
+            ).group(1)
         }
 
     @property
-    def title(self):
+    def title(self) -> str:
+        """
+        Returns the title of the short video.
+
+        Returns:
+            str: Title of the short video.
+        """
         return _extract_og("title", self._html)
 
     @property
-    def description(self):
+    def description(self) -> str:
+        """
+        Returns the description of the short video.
+
+        Returns:
+            str: Description of the short video.
+        """
         return _extract_og("description", self._html)
 
     @property
-    def thumbnail(self):
+    def thumbnail(self) -> str:
+        """
+        Returns the thumbnail of the short video.
+
+        Returns:
+            str: Thumbnail url of the short video.
+        """
         return _extract_og("image", self._html)
 
     @property
-    def date(self):
+    def date(self) -> str:
+        """
+        Returns the date of publishing of the short video.
+
+        Returns:
+            str: Date of publishing of the short video.
+        """
         return _extract_itemprop("datePublished", self._html)
 
     @property
-    def regions_allowed(self):
+    def regions_allowed(self) -> List[str]:
+        """
+        Returns the regions allowed for the short video.
+
+        Returns:
+            List[str]: List of regions allowed for the short video.
+        """
         return _extract_itemprop("regionsAllowed", self._html).split(",")
 
     @property
-    def genre(self):
+    def genre(self) -> str:
+        """
+        Returns the genre of the short video.
+
+        Returns:
+            str: Genre of the short video.
+        """
         return _extract_itemprop("genre", self._html)
 
     @property
-    def views(self):
+    def views(self) -> int:
+        """
+        Returns the view count of the short video.
+
+        Returns:
+            int: View count of the short video.
+        """
         matched = re.search(r'"views":{"simpleText":"(.*?) views"},', self._html)
         return int(matched.group(1).replace(",", ""))
 
     @property
-    def likes(self):
+    def likes(self) -> str:
+        """
+        Returns the likes count of the short video.
+
+        Returns:
+            str: Likes count of the short video.
+        """
         matched = re.search(
             r'\[{"factoidRenderer":{"value":{"simpleText":"(.*?)"}', self._html
         )
